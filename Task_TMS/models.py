1f34ae8924ee22a72062fff58aa2bba4148adf93
Aakash Rathor <aakashrathor2019@gmail.com>
2024-12-06 18:30:42 +0530

    Creating Models

--- Task_TMS/models.py
+++ Task_TMS/models.py
@@ -1,13 +1,71 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser , Group, Permission
+from .managers import UserManager
 
-#Cutomize abs user for email 
 class User(AbstractUser):
-  email= models.EmailField(unique=True)
+    username = None
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=200, default='', blank=True)
+    last_name = models.CharField(max_length=100, null=True, default='')
 
-  def __str__(self):
-    return self.username
-  
+    groups = models.ManyToManyField(Group, related_name="customuser_set", blank=True)
+    user_permissions = models.ManyToManyField(Permission, related_name="customuser_set", blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
 
 
- 
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('In Progress', 'In Progress'),
+        ('Completed', 'Completed'),
+    ]
+    
+    PRIORITY_CHOICES = [
+        (1, 'High'),
+        (2, 'Medium'),
+        (3, 'Low'),
+    ]
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    assignee = models.ForeignKey(User, on_delete=models.CASCADE, related_name='assigned_tasks')
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_tasks')
+    due_date = models.DateField()
+    priority = models.IntegerField(choices=PRIORITY_CHOICES, default=1)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='Pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.task.title}"
+
+class Notification(models.Model):
+    NOTIFY_CHOICES = [
+        ('Assigned', 'Assigned'),
+        ('Updated', 'Updated'),
+        ('Completed', 'Completed'),
+    ]
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='notifications')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
+    message = models.CharField(max_length=255)
+    type = models.CharField(choices=NOTIFY_CHOICES, max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.type} Notification for {self.task.title}"
